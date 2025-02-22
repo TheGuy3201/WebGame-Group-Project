2ca3ceef3b015511e1977017832f39818761bdb4
Trevor <trevormyles1@gmail.com>
2025-02-22 14:13:48 -0500

    Enemy Navigation

--- WebGameProject/Assets/_Project/Scripts/EnemyNavigation.cs
+++ WebGameProject/Assets/_Project/Scripts/EnemyNavigation.cs
@@ -5,19 +5,41 @@ namespace WebGame397
 {
     public class EnemyNavigation : MonoBehaviour
     {
-
         private NavMeshAgent agent;
         private Transform player;
+
+        [SerializeField] private float stoppingDistance = 1.5f; // Distance to stop
 
         private void Awake()
         {
             agent = GetComponent<NavMeshAgent>();
-            player = GameObject.FindWithTag("Player").transform;
+            GameObject playerObj = GameObject.FindWithTag("Player");
+
+            if (playerObj != null)
+            {
+                player = playerObj.transform;
+            }
+            else
+            {
+                Debug.LogError("Player not found! Make sure the player has the 'Player' tag.");
+            }
         }
 
-        void Update()
+        private void Update()
         {
-            agent.destination = player.position;
+            if (player != null && agent != null && agent.isActiveAndEnabled && agent.isOnNavMesh)
+            {
+                float distance = Vector3.Distance(transform.position, player.position);
+
+                if (distance > stoppingDistance)
+                {
+                    agent.SetDestination(player.position);
+                }
+                else
+                {
+                    agent.ResetPath(); // Stop moving if too close
+                }
+            }
         }
     }
 }
